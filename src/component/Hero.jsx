731e8d19794004b7bccedbba57c56b421369d7f0
Moyuri Akther <75539860+moyuriakther@users.noreply.github.com>
2024-09-27 18:12:14 +0600

    add react version

---
+++ src/component/Hero.jsx
@@ -0,0 +1,12 @@
+
+// eslint-disable-next-line react/prop-types
+export default function Hero() {
+  return (
+    <section className="bg-cover bg-center h-screen flex justify-center items-center text-white" style={{ backgroundImage: `url('https://demo.xpeedstudio.com/marketov2/home5/wp-content/uploads/sites/5/2021/06/vegetables1-1.png')` }}>
+        <div className="bg-black bg-opacity-50 p-6 rounded-lg text-center">
+            <h1 className="text-4xl md: text-6xl">Welcome to Our Website</h1>
+            <p className="mt-4 text-lg">Creating responsive web pages with HTML, CSS, and JavaScript</p>
+        </div>
+    </section>
+  )
+}
